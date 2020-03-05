9488989529813064caa41d7c64b4263ca142a1d1
Ellen Park <ellenpark781@gmail.com>
2020-03-05 17:19:35 -0500

    push

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,11 @@
 const FILES_TO_CACHE = [
     "/", 
     "/db.js",
-    "icons/icon-192x192.png",
-    "icons/icon-512x512.png",
-    "index.js",
-    "manifest.json",
-    "styles.css"
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/index.js",
+    "/manifest.json",
+    "/styles.css"
   ];
 
 const CACHE_NAME = "static-cache-v2";
